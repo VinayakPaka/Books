2575a67e456638f703cd73fd9590b15e44755816
Vinayak Paka <vinayakpaka22@gmail.com>
2026-01-05 13:20:00 +0530

    fix(server): enable cors for netlify frontend

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,8 +6,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://bookmanagementlive.netlify.app'],
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
+    methods: 'GET,PUT,POST,DELETE,OPTIONS',
+  });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
